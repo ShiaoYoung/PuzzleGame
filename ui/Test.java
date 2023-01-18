5ad4158656e08c341573edc659ac6b14301c71f1
ShiaoYoung <117451424+ShiaoYoung@users.noreply.github.com>
2023-01-18 22:36:03 +0800

    Add files via upload

---
+++ ui/Test.java
@@ -0,0 +1,22 @@
+package ui;
+
+import javax.swing.*;
+
+public class Test {
+    public static void main(String[] args) {
+        //1. 创建一个游戏的主界面
+        JFrame gameJFrame = new JFrame();
+        gameJFrame.setSize(603,680);
+        gameJFrame.setVisible(true);
+
+        //2. 创建登陆界面
+        JFrame loginJFrame = new JFrame();
+        loginJFrame.setSize(488,430);
+        loginJFrame.setVisible(true);
+
+        //3. 创建注册界面
+        JFrame registerJFrame = new JFrame();
+        registerJFrame.setSize(488,500);
+        registerJFrame.setVisible(true);
+    }
+}
